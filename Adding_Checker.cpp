e57c8ad552e7bbfbc1b65fe7d899339ff160980e
jsfkmrocks <jsfkmrocks@gmail.com>
2023-06-14 14:07:59 -0400

    Add files via upload

---
+++ Adding_Checker.cpp
@@ -0,0 +1,35 @@
+// Jordan Brown - Adding Checker - 9/10/19
+
+//Setup
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+//Main Routine
+int main ()
+
+{
+	//Initialize Variables
+	int num1,num2,ac_sum,user_sum;
+	
+	//Determine 1 & 2
+	num1 = (rand()%900)+100;
+	num2 = (rand()%900)+100;
+	
+	//Output and gather user inputs
+	cout << "\n   " << num1 << endl;
+	cout << " + " << num2 << endl;
+	cout << "_____\n";
+	cout << "\n\nEnter the sum: ";
+	cin >> user_sum;
+	
+	//Calculate
+	ac_sum = num1+num2;
+	
+	//Compare & Output
+	if(user_sum == ac_sum)
+		cout << "\nYou are correct!\n"<< endl;
+	else cout << "\nWrong, the sum is " << ac_sum << "\n" << endl;
+	
+}
+
